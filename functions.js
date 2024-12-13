cc1c0da63f6f41ce2df245e7421c7b337056d2a5
EidvydasJ <eidvydas.jenkinas@stud.vilniustech.lt>
2024-12-13 10:31:10 +0200

    atliktas 11 laboratorinis darbas

--- functions.js
+++ functions.js
@@ -37,3 +37,16 @@ function topFunction() {
         behavior: 'smooth'
     });
 }
+
+function UpdateClock() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = timeString;
+}
+UpdateClock();
+setInterval(UpdateClock, 1000);
